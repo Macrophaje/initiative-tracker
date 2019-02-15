eede040d0127cb919d7e11eec9dfa80eff081a7e
Duncan MacLeod <djmacleod621@gmail.com>
2019-02-14 18:39:02 -0800

    Updates

---
+++ precache-manifest.ddc5e986b01901ea26e5ea3c3457b384.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "773b363a5779b5fd55ba",
+    "url": "/initiative-tracker/static/js/runtime~main.773b363a.js"
+  },
+  {
+    "revision": "3923cba535f8728fd297",
+    "url": "/initiative-tracker/static/js/main.3923cba5.chunk.js"
+  },
+  {
+    "revision": "ec87eac21685f4bee71f",
+    "url": "/initiative-tracker/static/js/2.ec87eac2.chunk.js"
+  },
+  {
+    "revision": "3923cba535f8728fd297",
+    "url": "/initiative-tracker/static/css/main.e7fbb6a8.chunk.css"
+  },
+  {
+    "revision": "bc6d00ceecc4d13fd2011b9541aa909b",
+    "url": "/initiative-tracker/index.html"
+  }
+];
